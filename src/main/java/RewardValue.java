26e14f257c22dcf78e738cc5774ac661f0330c83
Aishwarya <114149922+WarHammerrr@users.noreply.github.com>
2024-09-01 16:58:31 +0530

    Reward class added, miles calculated properly

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,24 @@
+public class RewardValue {
+
+    private double cashValue;
+    private static double constant=0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+    public RewardValue(int miles){
+    this.cashValue=convertMilestoCash(miles);
+    }
+    public double getCashValue() {
+        return cashValue;
+    }
+    public int getMilesValue() {
+     int result=(int)(cashValue*constant);
+     return result;
+    }
+    public static double convertMilestoCash(int miles){
+        double res=(miles/constant);
+        return res;
+    }
+
+}
